c9f546fd1f09cc53078f2bde9c99ba10eb277f4d
F Grayson Meindl <meindlgrayson@gmail.com>
2017-03-10 13:25:05 -0800

    added project objects and shit

--- projects.js
+++ projects.js
@@ -1,3 +1,26 @@
-var beerBuddies = {
-  
-}
+var projArray = [
+
+  {
+    name: 'Beer Buddies',
+    projUrl: 'https://meindlgrayson.github.io/beer_buddies/',
+    description: 'Interactive trivia drinking game that can be played by up to four users at once.',
+    author: 'Grayson Meindl, Keeley Hammond, Morgan Fogarty, Jade Rager',
+    thumb: 'coming soon'
+  },
+
+  {
+    name: 'Bus Mall',
+    projUrl: 'https://meindlgrayson.github.io/bus-mall/',
+    description: 'Focus group activity where participants click the product most likely to purchase, with results displayed at the end.',
+    author: 'Grayson Meindl',
+    thumb: 'coming soon'
+  },
+
+  {
+    name: 'Salmon Cookies',
+    projUrl: 'https://meindlgrayson.github.io/salmon_cookies/',
+    description: 'Website for cookie-chain that tracks hypothetical sales based on store info.',
+    author: 'Grayson Meindl',
+    thumb: 'coming soon'
+  }
+];
